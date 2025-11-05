7dbb7ea36bacb69f6ebb99c539a1a2f86748dc17
Budhayan Chakraborty <budhayanc2005@gmail.com>
2025-11-05 22:40:00 +0530

    chore: Reduce load for load testing

--- run_bench.sh
+++ run_bench.sh
@@ -4,7 +4,7 @@ URL="http://localhost:8080"
 
 
 echo "Starting load test for order submission..."
-wrk -t3 -c70 -d15s -s loadtest/post_order.lua $URL #only testing the post order endpoint since it is the primary endpoint.
+wrk -t3 -c50 -d15s -s ./loadtest/post_order.lua $URL #only testing the post order endpoint since it is the primary endpoint.
 
 echo -e "\nFetching engine metrics..."
 curl -s $URL/metrics | jq
